16bcba82b32d96b7b3fdadf408d4b0ee15646c68
Ravi Udit <raviudit@gmail.com>
2022-06-06 16:04:31 -0400

    Query 2 finished.

--- HotelSchema/RaviUdit-HotelQueries.sql
+++ HotelSchema/RaviUdit-HotelQueries.sql
@@ -21,3 +21,26 @@ SELECT RoomReservation.ReservationNumber,
 -- 17	303	Bettyann	Seery	2023-07-28	2023-07-29
 
 -- 2. Write a query that returns a list of all reservations for rooms with a jacuzzi, displaying the guest's name, the room number, and the dates of the reservation.
+SELECT RoomReservation.ReservationNumber,
+	   Guests.GuestFirstName, 
+       Guests.GuestLastName, 
+       RoomReservation.RoomNumber,
+       RoomReservation.StartDate,
+       RoomReservation.EndDate
+       FROM RoomReservation
+       INNER JOIN GuestReservation ON RoomReservation.ReservationNumber = GuestReservation.ReservationNumber
+       INNER JOIN Guests ON GuestReservation.GuestID = Guests.GuestID
+       INNER JOIN Rooms ON RoomReservation.RoomNumber = Rooms.RoomNumber 
+       WHERE Rooms.Amenities LIKE '%Jacuzzi%';
+       
+-- Results 
+-- 2	Bettyann	Seery	203	2023-02-05	2023-02-10
+-- 3	Duane 	Cullison	305	2023-02-22	2023-02-24
+-- 4	Karie	Yang	201	2023-03-06	2023-03-07
+-- 5	Ravi	Udit	307	2023-03-17	2023-03-20
+-- 9	Walter	Holaway	301	2023-04-09	2023-04-13
+-- 10	Wilfred	Vise	207	2023-04-23	2023-04-24
+-- 14	Ravi	Udit	205	2023-06-28	2023-07-02
+-- 17	Bettyann	Seery	303	2023-07-28	2023-07-29
+-- 18	Bettyann	Seery	305	2023-08-30	2023-09-01
+-- 20	Karie	Yang	203	2023-09-13	2023-09-15
